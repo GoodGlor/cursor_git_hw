dcc1697bcc304136ee8cf78d7295c698d07de334
Artem Levkovych <artem.levkovich0011@gmail.com>
2021-03-13 22:31:32 +0200

    Add HW7.py | finished

---
+++ HW7.py
@@ -0,0 +1,38 @@
+# Task 1
+
+task1 = open('task_1.txt')
+full_lst = [lst.replace('\n', '') for lst in task1]
+dic = {full_lst[index]: full_lst[index + 1] for index in range(0, len(full_lst), 2)}
+task1.close()
+
+# Task 2
+
+import pickle
+
+with open('task2', 'rb') as task2:
+    unbin = pickle.load(task2)
+    total = sum(unbin) / len(unbin)
+
+# Task 3
+
+
+import xlsxwriter
+
+
+class OpenXlsx:
+    def __init__(self, file):
+        self.filename = open(file)
+        self.workbook = xlsxwriter.Workbook(file)
+        self.sheet = self.workbook.add_worksheet()
+
+    def __enter__(self):
+        return self.sheet
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.workbook.close()
+        self.filename.close()
+
+
+with OpenXlsx('cursor.xlsx') as f:
+    f.write("B3", 'Cursor')
+
