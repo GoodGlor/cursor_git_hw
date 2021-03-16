401e47fca5480bc4614654fa5508fa8c34fa2f48
Artem Levkovich <artem.levkovich0011@gmail.com>
2021-03-16 18:27:20 +0200

    Modify HW7.py | add task 1_2

--- HW7.py
+++ HW7.py
@@ -1,9 +1,13 @@
 # Task 1
 
-task1 = open('task_1.txt')
+task1 = open('task1.txt', 'r')
 full_lst = [lst.replace('\n', '') for lst in task1]
 dic = {full_lst[index]: full_lst[index + 1] for index in range(0, len(full_lst), 2)}
 task1.close()
+task1_2 = open('task1.txt', 'w')
+text = [task1_2.write(f'{line} \n') for line in dic.values()]
+task1_2.close()
+
 
 # Task 2
 
@@ -35,4 +39,3 @@ class OpenXlsx:
 
 with OpenXlsx('cursor.xlsx') as f:
     f.write("B3", 'Cursor')
-
