3fe7500135cb25d697291c0772781558c595143c
Artem Levkovich <artem.levkovich0011@gmail.com>
2021-03-18 23:22:19 +0200

    Modify HW8

--- HW8.py
+++ HW8.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 import random
 from typing import Dict, Any
 from abc import ABC, abstractmethod
+import time
+import uuid
 
 predators_list = ['Tiger', 'Crocodile', 'Bear', 'Shark', 'Komodo', 'Leopard', 'Panther', 'Cobra',
                   'Wildcat', 'Gharial', 'Eagle', 'Cheetah']
@@ -10,10 +12,12 @@ herbivorous_list = ['Beaver', 'Bison', 'Buffalo', 'Camel', 'Goose', 'Oriole', 'A
 
 different = {'predator': 0, 'herbivorous': 1}
 
+AnyAnimal: Any[Herbivores, Predator]
+
 
 class Animal(ABC):
-    def __init__(self, type_animal: int, name: str, power: int, speed: int):
-        self.type_animal = type_animal
+    def __init__(self, name: str, power: int, speed: int):
+        self.id = None
         self.name = name
         self.power = power
         self.speed = speed
@@ -23,87 +27,90 @@ class Animal(ABC):
         pass
 
     @abstractmethod
-    def state(self, forest: Forest):
+    def state(self, animal: AnyAnimal):
         pass
 
 
 class Herbivores(Animal):
-    def __init__(self, type_animal, name, power, speed):
-        super(Herbivores, self).__init__(type_animal, name, power, speed)
+    def __init__(self, name, power, speed):
+        super(Herbivores, self).__init__(name, power, speed)
 
     def state(self, forest: Forest):
         return f'{self.name} power {self.power} speed {self.speed}'
 
     def eat(self, forest: Forest):
+        print(f'Eating {self.name} ')
         percent = self.power * 0.5
-        self.power += percent
+        self.power += round(percent)
         if self.power > 100:
             difference = self.power - 100
             self.power = self.power - difference
 
 
 class Predator(Animal):
-    def __init__(self, type_animal, name, power, speed):
-        super(Predator, self).__init__(type_animal, name, power, speed)
+    def __init__(self, name, power, speed):
+        super(Predator, self).__init__(name, power, speed)
 
     def state(self, forest: Forest):
         return f'{self.name} power {self.power} speed {self.speed}'
 
     def eat(self, forest: Forest):
-        percent = self.power * 0.5
-        self.power += percent
-        if self.power > 100:
-            difference = self.power - 100
-            self.power = self.power - difference
+        for one in forest.animals.values():
+            if one.id == self.id:
+                return False
+            print(f'{self.name} fight with {one.name}')
+            if one.speed > self.speed:
+                self.power = self.power - (round(self.power*0.3))
+                return False
+            elif one.power > self.power:
+                self.power = self.power - (round(self.power * 0.3))
 
-
-AnyAnimal: Any[Herbivores, Predator]
 
 
 class Forest:
 
     def __init__(self):
         self.animals: Dict[str, AnyAnimal] = dict()
-        self.list_animals = []
 
     def add_animal(self, animal: AnyAnimal):
-        #self.list_animals.append([dict(type_of_animal=animal.type_animal, name=animal.name, power=animal.power, speed=animal.speed)])
-        self.list_animals.append([animal])
+        return self.animals.update({animal.id: animal})
 
-        return self.list_animals
+    def remove_animal(self):
+        pass
 
-
-
-    def remove_animal(self, animal: AnyAnimal):
+    def any_predator(self):
         pass
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        return random.choice(self.list_animals)
+        return self.animals
 
 
 def animal_generator():
     while True:
-        predator = Predator(0, random.choice(predators_list), random.randint(25, 100), random.randint(25, 100))
-        herbivore = Herbivores(1, random.choice(herbivorous_list), random.randint(25, 100),
-                               random.randint(25, 100))
-        yield random.choice([predator, herbivore])
+        any_animal = random.choice(
+            (Predator(random.choice(predators_list), random.randint(25, 100), random.randint(25, 100)),
+             Herbivores(random.choice(herbivorous_list), random.randint(25, 100), random.randint(25, 100))))
+        any_animal.id = uuid.uuid4()
+        yield any_animal
 
 
 nature = animal_generator()
 
 forest = Forest()
-for i in range(10):
+for i in range(2):
     animal = next(nature)
     forest.add_animal(animal)
+print(forest.animals.values())
 
-for an in forest.list_animals:
-    for ani in an:
-        print(ani.state(forest=forest))
-        # if ani.name == ani.name:
-        #     ani.
+while True:
+    for x in forest.animals.values():
+        x.eat(forest=forest)
+        print(x.state(forest=forest))
+    time.sleep(2)
+
 
 
 
